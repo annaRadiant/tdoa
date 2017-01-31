502b6301696a9472a7d59b865d47a5e555648235
annaRadiant <aheithier@radiantblue.com>
2017-01-31 12:54:53 -0500

    added more code

--- tdoaGAI.java
+++ tdoaGAI.java
@@ -1,1 +1,15 @@
+/* Anna add package here */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import nxm.sys.inc.Constants;
+import nxm.sys.lib.Position;
+import nxm.sys.lib.Transform;
+import Jama.EigenvalueDecomposition;
+import.Matrix;
+
+public class GeoUtil {
+  public static final double SPEED_OF_LIGHT = 299792458
+  public static final double EARTH_FLATTENING_FACTOR = 1/298.257223563d
